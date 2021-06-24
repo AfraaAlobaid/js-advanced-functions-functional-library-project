60490e91c7f5b4dbbdeb26ed561a5da67bf9cbdd
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-06-24 16:03:36 +0930

    add reduce implementation

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const fi = (function() {
+const fi = (function () {
   return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+    libraryMethod: function () {
+      return "Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0";
     },
 
-    each: function(iterable, callback) {
-      for (const index in iterable){
+    each: function (iterable, callback) {
+      for (const index in iterable) {
         callback(iterable[index]);
       }
       return iterable;
     },
 
-    map: function(iterable, callback) {
+    map: function (iterable, callback) {
       let result = [];
-      //let count = 0;
-      for (const index in iterable){
+      for (const index in iterable) {
         result.push(callback(iterable[index]));
       }
       return result;
     },
 
-    reduce: function() {
-
+    reduce: function (collection, callback, acc) {
+      let i;
+      if (acc === undefined) {
+        acc = collection[0];
+        i = 1;
+      } else {
+        i = 0;
+        acc = acc;
+      }
+      for (i; i < collection.length; i++) {
+        acc = callback(acc, collection[i], collection);
+      }
+      return acc;
     },
 
-    functions: function() {
+    functions: function () {},
+  };
+})();
 
-    },
+fi.libraryMethod();
 
-
-  }
-})()
-
-fi.libraryMethod()
