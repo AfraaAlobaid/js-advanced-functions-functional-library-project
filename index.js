b2463ac1842e69c24ac3e40f175e2a26c6f900f4
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-06-24 15:01:49 +0930

    add each implementation

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(iterable, callback) {
+      for (const index in iterable){
+        callback(iterable[index]);
+      }
+      return iterable;
     },
 
-    map: function() {
-
+    map: function(iterable, callback) {
+      let resultIterable;
+      for (const index in iterable){
+        resultIterable[index] = callback(iterable[index]);
+      }
+      return resultIterable;
     },
 
     reduce: function() {
